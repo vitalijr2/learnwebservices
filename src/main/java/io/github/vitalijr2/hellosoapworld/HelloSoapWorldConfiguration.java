c72fb7cf636dafbeab3644511697867375fc43ff
Vitalij Berdinskih <vitalij_r2@outlook.com>
2023-09-22 19:41:32 +0300

    Customise the "User-Agent" header

--- src/main/java/io/github/vitalijr2/hellosoapworld/HelloSoapWorldConfiguration.java
+++ src/main/java/io/github/vitalijr2/hellosoapworld/HelloSoapWorldConfiguration.java
@@ -2,6 +2,11 @@ package io.github.vitalijr2.hellosoapworld;
 
 import io.github.vitalijr2.hellosoapworld.client.HelloEndpoint;
 import io.github.vitalijr2.hellosoapworld.client.HelloEndpointService;
+import jakarta.xml.ws.BindingProvider;
+import jakarta.xml.ws.handler.MessageContext;
+import java.util.HashMap;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,8 +19,16 @@ public class HelloSoapWorldConfiguration {
   }
 
   @Bean
-  public HelloEndpoint helloEndpoint(HelloEndpointService service) {
-    return service.getHelloEndpointPort();
+  public HelloEndpoint helloEndpoint(HelloEndpointService service,
+      @Value("${hello-client.user-agent}") String userAgent) {
+    var helloEndpoint = service.getHelloEndpointPort();
+    var headers = new HashMap<String, List<String>>();
+
+    headers.put("User-Agent", List.of(userAgent));
+    ((BindingProvider) helloEndpoint).getRequestContext()
+        .put(MessageContext.HTTP_REQUEST_HEADERS, headers);
+
+    return helloEndpoint;
   }
 
 }
